3c3e2628855cdceb90963c5f53a8e6fa902f3cba
maxdiploma2025 <maxdiploma2025@gmail.com>
2025-05-14 12:19:30 +0300

    fix

--- AiServer.js
+++ AiServer.js
@@ -9,43 +9,49 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8089;
 
-// Enable CORS
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 app.use(express.json());
 
 const HUGGING_FACE_API_KEY = process.env.HUGGING_FACE_API_KEY;
 const HUGGING_FACE_TEXT_MODEL_URL = process.env.HUGGING_FACE_TEXT_MODEL_URL;
 const HUGGING_FACE_IMAGE_MODEL_URL = process.env.HUGGING_FACE_IMAGE_MODEL_URL;
 
-async function queryImage(prompt, parameters = {}) {
-  try {
-    const response = await axios.post(
-      `https://api-inference.huggingface.co/models/${HUGGING_FACE_IMAGE_MODEL_URL}`,
-      {
-        inputs: prompt,
-        parameters: parameters,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${HUGGING_FACE_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        responseType: "arraybuffer",
-        timeout: 210000,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching response:", error);
-    throw error;
-  }
-}
+async function queryImage(data, retryCount = 0) {
+  const MAX_RETRIES = 3;
 
+  const response = await fetch(
+    `https://router.huggingface.co/hf-inference/models/${HUGGING_FACE_IMAGE_MODEL_URL}`,
+    {
+      headers: {
+        Authorization: `Bearer ${HUGGING_FACE_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+
+  const contentType = response.headers.get("content-type") || "";
+  const buffer = await response.arrayBuffer();
+  const base64 = Buffer.from(buffer).toString("base64");
+  const dataUrl = `data:${contentType};base64,${base64}`;
+
+  if (contentType.startsWith("text/html")) {
+    console.warn(`Received HTML response. Retry attempt ${retryCount + 1}`);
+
+    if (retryCount < MAX_RETRIES) {
+      return queryImage(data, retryCount + 1);
+    } else {
+      throw new Error("Failed to generate image after multiple retries.");
+    }
+  }
+
+  return dataUrl;
+}
 
 async function queryText(userMessage) {
   const systemPrompt = "You are an ai assistant for advertisement, u must answers for questions only about advertisemet";
@@ -78,29 +84,21 @@ async function queryText(userMessage) {
 }
 
 app.post("/image", async (req, res) => {
-  const { prompt, width = 512, height = 512 } = req.body;
+  const { prompt, width, height } = req.body;
 
   if (!prompt) {
     return res.status(400).json({ error: "Prompt is required." });
   }
-
-  const parameters = {
-    width,
-    height,
-  }
-
   try {
-    const imageData = await queryImage(prompt, parameters);
-
-    const base64Image = Buffer.from(imageData, "binary").toString("base64");
-    const dataUrl = `data:image/png;base64,${base64Image}`;
+    const imageDataUrl = await queryImage({inputs: prompt, parameters: {width, height}});
 
     res.json({
       message: "Image generated successfully.",
-      image: dataUrl,
+      image: imageDataUrl,
     });
   } catch (error) {
-    res.status(500).json({ error: "Error fetching the image." });
+    res.status(500).json({ error: "Error generating image." });
+    console.log(error);
   }
 });
 
